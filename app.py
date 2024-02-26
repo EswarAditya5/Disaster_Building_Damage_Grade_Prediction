85bd2d8e97fc99e8ad601a87384dc861b0576ea5
Eswar Aditya <142793639+EswarAditya5@users.noreply.github.com>
2024-02-26 13:59:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def predict(data):
 # Prediction
 if st.button('Predict'):
     prediction = predict(np.array(X))
-    prediction = st.text(prediction[0])
+    prediction = (prediction[0])
     if prediction == 0:
         st.markdown(f"The building damage condition is Low")
     elif prediction == 1:
